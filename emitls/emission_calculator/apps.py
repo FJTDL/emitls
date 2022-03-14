81de2a4bb5a1e7780a9a34dc226bbc15c984a31e
FJTDL <finn.jt.delange@gmail.com>
2022-03-15 10:21:37 +1300

    Fixed the loading bug and created a basic landing page

--- emitls/emission_calculator/apps.py
+++ emitls/emission_calculator/apps.py
@@ -2,5 +2,4 @@ from django.apps import AppConfig
 
 
 class EmissionCalculatorConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
     name = 'emission_calculator'
--- emitls/emitls/settings.py
+++ emitls/emitls/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    "emission_calculator.apps.Emission_calculatorConfig"
+    'emission_calculator',
 ]
 
 MIDDLEWARE = [
