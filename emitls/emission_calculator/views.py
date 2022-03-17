881f2bd87b0b168feb4fa39e8b1565e2456e72e7
FJTDL <finn.jt.delange@gmail.com>
2022-03-17 15:41:13 +1300

    Added the calculator page (score.html) as appropriated code from one of my past projects. Will need to be refactored.

--- emitls/emission_calculator/views.py
+++ emitls/emission_calculator/views.py
@@ -1,5 +1,18 @@
 from django.shortcuts import render
+from django import forms
+from django.http.response import HttpResponseRedirect
+
+class NewTaskForm(forms.Form):
+  humanNumber = forms.IntegerField(label="Walk/cycle", min_value=0, max_value=100)
+  publicNumber = forms.IntegerField(label="Public transport", min_value=0, max_value=100)
+  privateNumber = forms.IntegerField(label="Private vehicle", min_value=0, max_value=100)
 
 # Create your views here.
 def home(request):
   return render(request, "emission_calculator/home.html")
+
+
+def score(request):
+  return render(request, "emission_calculator/score.html", {
+        "form": NewTaskForm()
+    })
