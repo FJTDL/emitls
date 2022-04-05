0c8eed186ef0491614cdb0c9e43e280b61467a13
FJTDL <finn.jt.delange@gmail.com>
2022-04-06 08:48:53 +1200

    Added refactored and fixed login/logout functionality. Began basic work on how to control the account page

--- emitls/emission_calculator/models.py
+++ emitls/emission_calculator/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Score(models.Model):
+    user = models.ForeignKey('User', on_delete=models.CASCADE)
+    subject = models.CharField(max_length=20)
+    score = models.IntegerField()
+
+class User(models.Model):
+    name = models.CharField(max_length=20)
+    age = models.IntegerField()
+    # score = models.ForeignKey(Score, on_delete=models.CASCADE)
--- emitls/emission_calculator/views.py
+++ emitls/emission_calculator/views.py
@@ -6,13 +6,7 @@ from django.contrib import messages
 from .forms import *
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth import login, authenticate, logout
-
-class YesNoForm(forms.Form):
-    electric_car = forms.TypedChoiceField(coerce=lambda x: x =='True', choices=((False, 'No'), (True, 'Yes')))
-    provider = forms.TypedChoiceField(coerce=lambda x: x =='True', choices=((False, 'No'), (True, 'Yes')))
-    reduce_reuse_recycle = forms.TypedChoiceField(coerce=lambda x: x =='True', choices=((False, 'No'), (True, 'Yes')))
-    budget = forms.IntegerField(label="budget", min_value=0)
-
+from .models import *
 
 # Create your views here.
 def home(request):
@@ -36,7 +30,7 @@ def provider(request):
 
 
 def reduce_reuse_recycle(request):
-  return(request, "emission_calculator/rrr.html")
+  return render(request, "emission_calculator/rrr.html")
 
 
 def score(request):
@@ -64,3 +58,51 @@ def output(request):
     "rrr": request.POST['reduce_reuse_recycle'],
     "budget": int(request.POST['budget']),
   })
+
+def register(request):
+	if request.method == "POST":
+		form = NewUserForm(request.POST)
+		if form.is_valid():
+			user = form.save()
+			login(request, user)
+			messages.success(request, "Registration successful." )
+			return redirect("calculator:options")
+		messages.error(request, "Unsuccessful registration. Invalid information.")
+	form = NewUserForm()
+	return render (request=request, template_name="emission_calculator/signup.html", context={"register_form":form})
+
+
+def login_request(request):
+	if request.method == "POST":
+		form = AuthenticationForm(request, data=request.POST)
+		if form.is_valid():
+			username = form.cleaned_data.get('username')
+			password = form.cleaned_data.get('password')
+			user = authenticate(username=username, password=password)
+			if user is not None:
+				login(request, user)
+				context = {"user": user}
+				messages.info(request, f"You are now logged in as {username}.")
+				return render(request, "emission_calculator/account.html", context)
+			else:
+				messages.error(request,"Invalid username or password.")
+		else:
+			messages.error(request,"Invalid username or password.")
+	form = AuthenticationForm()
+	return render(request=request, template_name="emission_calculator/login.html", context={"login_form":form})
+
+
+def logout_request(request):
+	logout(request)
+	messages.info(request, "You have successfully logged out.") 
+	return redirect("calculator:")
+
+
+def my_account(request):
+	scores = Score.objects.order_by('user')
+	return render(request, "emission_calculator/account.html", {'scores': scores})
+
+
+def my_account(request):
+	scores = Score.objects.order_by('user')
+	return render(request, "emission_calculator/account.html", {'scores': scores})
